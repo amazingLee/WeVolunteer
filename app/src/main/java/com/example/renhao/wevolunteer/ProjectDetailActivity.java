227221820337b2448cb89793f452d12cec3fddad
Ge <493944330@qq.com>
2016-08-26 15:46:53 +0800

    分享界面上传，build有修改注意，分享内容未设置

--- app/src/main/java/com/example/renhao/wevolunteer/ProjectDetailActivity.java
+++ app/src/main/java/com/example/renhao/wevolunteer/ProjectDetailActivity.java
@@ -58,6 +58,8 @@ import java.util.regex.Pattern;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 /**
  * 项目名称：WeVolunteer
@@ -611,13 +613,14 @@ public class ProjectDetailActivity extends BaseActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_projectdetail_share:
-                Holder share = new ViewHolder(R.layout.dialog_share);
-                DialogPlus dialogShare = DialogPlus.newDialog(this)
-                        .setContentHolder(share)
-                        .setCancelable(true)
-                        .setGravity(Gravity.BOTTOM)
-                        .create();
-                dialogShare.show();
+//                Holder share = new ViewHolder(R.layout.dialog_share);
+//                DialogPlus dialogShare = DialogPlus.newDialog(this)
+//                        .setContentHolder(share)
+//                        .setCancelable(true)
+//                        .setGravity(Gravity.BOTTOM)
+//                        .create();
+//                dialogShare.show();
+                showShare();
                 break;
             case R.id.btn_projectdetail_apply:
                 siginIn();
@@ -856,5 +859,33 @@ public class ProjectDetailActivity extends BaseActivity {
             finish();*/
     }
 
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.ssdk_oks_share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
+    }
 
 }
